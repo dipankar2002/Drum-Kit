9e8dbc258032962e070d97ec9b228795826cf48f
Dipankar Ganguly <dipankarganguly15@gmail.com>
2024-01-10 01:03:13 +0530

    Adding the JavaScript file

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+var buttonLength = document.querySelectorAll(".drum").length;
+for(var i = 0;i < buttonLength; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function tap() {
+        var key = this.innerHTML;
+        if(key == "w") {
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+        } else if(key == "a") {
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+        } else if(key == "s") {
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+        } else if(key == "d") {
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+        } else if(key == "j") {
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+        } else if(key == "k") {
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+        } else if(key == "l") {
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+        }
+    });
+
+    document.querySelectorAll("button")[i].addEventListener("keydown", function key(keypress) {
+        var key = keypress.key;
+        if(key == "w") {
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+        } else if(key == "a") {
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+        } else if(key == "s") {
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+        } else if(key == "d") {
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+        } else if(key == "j") {
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+        } else if(key == "k") {
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+        } else if(key == "l") {
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+        }
+    })
+}
